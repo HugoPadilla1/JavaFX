db2e217853c5804e104316747e574ab2576e5a94
HugoPadilla1 <hpadilla@ggc.edu>
2024-09-15 21:04:36 -0400

    most update

--- src/TeamControllerFXML.java
+++ src/TeamControllerFXML.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
 
 import java.awt.*;
 
@@ -25,7 +27,39 @@ public class TeamControllerFXML {
     private TextField returningTF;
 
     @FXML
-    public void generate(){
+    public void generate() {
+        try {
+            String teamName = nameTF.getText();
+            int teamWins = Integer.parseInt(winsTF.getText());
+            int teamLosses = Integer.parseInt(lossesTF.getText());
+            String mascot = mascotTF.getText();
+            int returningPlayers = Integer.parseInt(returningTF.getText());
 
+            Team team = new Team(teamName, teamWins, teamLosses, mascot, returningPlayers);
+            showAlert(Alert.AlertType.INFORMATION, "Success", "Team was generated.");
+        } catch (NumberFormatException e){
+            showAlert(Alert.AlertType.ERROR, "Invalid Input", "Wins, Losses, and returning Players are integers.");
+        } catch (TeamValidationException e){
+            showAlert(Alert.AlertType.ERROR, "Validation Error!", e.getMessage());
+        }
+    }
+
+    @FXML
+    public void cancel() {
+        nameTF.clear();
+        winsTF.clear();
+        lossesTF.clear();
+        mascotTF.clear();
+        returningTF.clear();
+
+        Stage stage = (Stage) nameTF.getScene().getWindow();
+        stage.close();
+    }
+
+    private void showAlert(Alert.AlertType type, String title, String message){
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
